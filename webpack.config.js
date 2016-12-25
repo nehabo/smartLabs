efa3e3295777bcf2c5a1a33ee2731f5b85c2beeb
nehabo <neha.bommareddy@gmail.com>
2016-12-25 15:54:59 +0530

    configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+// entry point(s) for app
+  entry: [
+    './app/main.js',
+  ],
+// transcompilers location
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      { test: /\.(jpg|png|gif|cur)$/, loaders: ['file-loader'] },
+    ],
+  },
+// output for bundled files
+  output: {
+    filename: './dist/index_bundle.js',
+    publicPath: '/',
+  },
+
+};
