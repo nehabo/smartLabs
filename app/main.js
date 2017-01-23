2dafa02e2cdb4f2a9f76ce7b1d5a64fb70495d43
nehabo <neha.bommareddy@gmail.com>
2017-01-23 13:29:45 +0530

    implements redux-logger middleware

--- app/main.js
+++ app/main.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, browserHistory, IndexRedirect } from 'react-router';
 import { persistStore, autoRehydrate } from 'redux-persist';
+import createLogger from 'redux-logger';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import Home from './modules/home/containers/home';
-import route from './modules/common/components/nav/page';
-import main from './modules/home/containers/main';
 import Search from './modules/home/containers/search';
 import Patient from './modules/home/containers/patient';
 import Address from './modules/home/containers/address';
 import Order from './modules/home/containers/order';
 import reducers from './modules/home/reducers/';
 
-
-const store = createStore(reducers, undefined, autoRehydrate());
+const logger = createLogger();
+const store = createStore(
+  reducers,
+  undefined,
+  compose(
+    autoRehydrate(),
+    applyMiddleware(logger),
+  ),
+);
 persistStore(store);
 
 const routes = [{
